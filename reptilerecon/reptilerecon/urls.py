957b18b8ddef843ca5ee0984c60677f19997f872
joaozenobio <joao.zenobio@aluno.ufop.edu.br>
2022-04-05 00:49:41 -0300

    deploying

--- reptilerecon/reptilerecon/urls.py
+++ reptilerecon/reptilerecon/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('YOLOv5/', include('YOLOv5.urls', namespace='YOLOv5_nm')),
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
