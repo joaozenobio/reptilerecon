641d0aa70939bfa17ce20c18cffb46baeb535980
joaozenobio <joao.zenobio@aluno.ufop.edu.br>
2022-06-09 20:29:39 -0300

    start reconfiguration

--- startServer.sh
+++ startServer.sh
@@ -2,11 +2,12 @@
 
 git submodule init
 git submodule update
-cd reptilerecon/yolov5 && pip install -r requirements.txt && cd ..
-python manage.py makemigrations
+pip install -r reptilerecon/yolov5/requirements.txt
+python reptilerecon/manage.py migrate
+python reptilerecon/manage.py makemigrations
 pip install django==4.0.3
 pip install opencv-python
 pip install plotly
 pip install torch==1.10.0+cu102 torchvision==0.11.0+cu102 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install daphne
-daphne -b 0.0.0.0 -p 8000 reptilerecon.asgi:application
+cd reptilerecon && daphne -b 0.0.0.0 -p 8000 reptilerecon.asgi:application
